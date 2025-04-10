87bc4183466b9abf46cf9633da0c8512672612c9
sanket Yadav <yadavsanket663@gmail.com>
2025-04-10 15:23:02 +0530

    Day 1 React Practice

---
+++ 01Project/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ProductTab from "./ProductTab"
+
+function App() {
+  return (
+    <>
+    <ProductTab />
+    </>
+  );
+}
+
+export default App;
---
+++ 01Project/src/Product.jsx
@@ -0,0 +1,11 @@
+import "./Product.css"
+function Product(){
+    return(
+        <div className="Product">
+            <h2>Name:Kisan Jam</h2>
+            <h3>Price:499</h3>
+            <h3>Store:D-Mart</h3>
+        </div>
+    )
+}
+export default Product;
