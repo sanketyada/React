3cdcff1bc11511b8c2dfbd410bade200106739eb
sanket Yadav <yadavsanket663@gmail.com>
2025-09-06 23:11:25 +0530

    Major Project Completed

--- MegaProject/src/components/index.js
+++ MegaProject/src/components/index.js
@@ -1,10 +1,9 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/container";
+import Container from "./container/Container";
 import Logo from "./Logo";
 import Button from "./Button";
 import Input from "./Input";
-import { logout } from "../store/authSlice";
 import Select from "./Select";
 import RTE from "./RTE";
 import Signup from "./SignUp";
